60c647feb829121237cefd9c9dedc5291822bcda
moureen mutinda <maurinemutinda72@gmail.com>
2025-02-05 08:08:21 +0300

    done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,23 @@
-// Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome', () => {
+  test('returns true for palindromes', () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  test('returns false for non-palindromes', () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  test('returns true for mixed-case palindromes', () => {
+    expect(isPalindrome("RaceCar")).toBe(true);
+  });
+
+  test('returns true for an empty string', () => {
+    expect(isPalindrome("")).toBe(true);
+  });
+
+
+ 
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+            return true;  // An empty string is considered a palindrome.
+      }
+  
+    const cleanedWord = word.toLowerCase();
+    return cleanedWord === cleanedWord.split('').reverse().join('');
+  }
+  
+  module.exports = { isPalindrome };
